007c46f77bd864aad7a1981a2d71e0751907ae6b
Milind Deore <milind.deore@privittytech.com>
2026-08-18 20:27:45 +0530

    fix: mac desktop signing, merge conflicts

--- packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
+++ packages/frontend/src/components/screens/MainScreen/MainScreen.tsx
@@ -597,29 +597,21 @@ function ChatNavButtons({ chat }: { chat: T.FullChat }) {
             styling='borderless'
             onClick={onRemoteAccessClick}
           >
-            {/* Use a network/computer icon; Icon component may not have 'network' so we use SVG */}
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width={18}
-              height={18}
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth={2}
-              strokeLinecap='round'
-              strokeLinejoin='round'
-              aria-hidden='true'
-              style={{
-                color: tunnelActive
-                  ? 'var(--colorSuccess, #2e7d32)'
-                  : undefined,
-              }}
+            <span
+              style={
+                tunnelActive
+                  ? ({
+                      '--icon-color': 'var(--colorSuccess, #2e7d32)',
+                    } as React.CSSProperties)
+                  : undefined
+              }
             >
-              {/* Monitor / display icon (matches Android ic_baseline_devices_24) */}
-              <rect x='2' y='3' width='20' height='14' rx='2' ry='2' />
-              <line x1='8' y1='21' x2='16' y2='21' />
-              <line x1='12' y1='17' x2='12' y2='21' />
-            </svg>
+              <Icon
+                coloring={tunnelActive ? 'iconColorCSSVar' : 'navbar'}
+                icon='network'
+                size={18}
+              />
+            </span>
           </Button>
         )}
         {/* Note that the `enableAVCallsV2` setting itself is hidden
